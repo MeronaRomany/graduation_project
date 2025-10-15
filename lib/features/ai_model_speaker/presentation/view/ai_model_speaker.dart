7c7797feea7cf70474cc7d7c2a6f76c1ad492ddc
Mark Maher <markmaher933@gmail.com>
2025-10-15 23:22:49 +0300

    Refactor comments in ai_model_speaker.dart for clarity and consistency

--- lib/features/ai_model_speaker/presentation/view/ai_model_speaker.dart
+++ lib/features/ai_model_speaker/presentation/view/ai_model_speaker.dart
@@ -1,11 +1,11 @@
-userIntruption() {
-  // Chaeck if the wisper is speaking now
-  // Check if the user Speaking now
+// userIntruption() {
+//   // Chaeck if the wisper is speaking now
+//   // Check if the user Speaking now
 
-  // Then Stop Wisper Speaking
-}
+//   // Then Stop Wisper Speaking
+// }
 
-userIsSpeaking() {
-  // Convert his audio to text
-  //If Converted Successfuly Send it to Gemini Model
-}
+// userIsSpeaking() {
+//   // Convert his audio to text
+//   //If Converted Successfuly Send it to Gemini Model
+// }
