aa97dc41bdb92df9929e44556bfb1ec5c1abbfd5
Afraym-Herz <afraym100herz@gmail.com>
2026-04-11 12:24:52 +0200

    feat: implement chat practice feature and configure asset generation in pubspec

---
+++ lib/features/chat_practice/screens/chat_practice_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_app/features/chat_practice/screens/widgets/chat_practice_view_body.dart';
+
+class ChatPracticeView extends StatelessWidget {
+  const ChatPracticeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const ChatPracticeViewBody());
+  }
+}
---
+++ lib/features/chat_practice/screens/widgets/chat_practice_view_body.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_app/core/utils/assets.dart';
+import 'package:graduation_app/features/home/presentation/view/widgets/floating_bottom_nav.dart';
+import 'package:graduation_app/features/chat_practice/screens/widgets/header_deleget.dart';
+import 'package:graduation_app/features/chat_practice/screens/widgets/ice_breaker_grid.dart';
+import 'package:graduation_app/features/chat_practice/screens/widgets/section_header.dart';
+import 'package:graduation_app/features/chat_practice/screens/widgets/user_card.dart';
+
+class ChatPracticeViewBody extends StatelessWidget {
+  const ChatPracticeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        extendBody: true,
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage(Assets.assetsImagesBackground),
+            fit: BoxFit.fill,
+          )),
+          child: Stack(
+            children: [
+              SafeArea(
+                child: CustomScrollView(
+                  slivers: [
+                    const HeaderDelegate(),
+                    const SliverToBoxAdapter(child: SizedBox(height: 20)),
+                    const SliverToBoxAdapter(
+                        child: SectionHeader(title: "Online Now", count: 24)),
+                    SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (context, index) => const UserCard(),
+                        childCount: 4,
+                      ),
+                    ),
+                    const SliverToBoxAdapter(
+                        child: SectionHeader(title: "Icebreakers")),
+                    const SliverToBoxAdapter(child: IcebreakerGrid()),
+                    const SliverToBoxAdapter(child: SizedBox(height: 120)),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: const FloatingBottomNav(),
+      ),
+    );
+  }
+}
