e7f484d5f928ebf6d9112398751111a9b1cc6ef0
Afraym-Herz <afraym100herz@gmail.com>
2026-04-11 12:23:33 +0200

    feat: implement chat practice and home view UI components with asset management support

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -9,38 +9,50 @@ class Assets {
 
   /// Assets for resourceGifsSpeaking
   /// assets/gifs/speaking.gif
-  static const String resourceGifsSpeaking = "assets/gifs/speaking.gif";
+  static const String assetsGifsSpeaking = "assets/gifs/speaking.gif";
 
-  /// Assets for resourceImagesBackground
+  /// Assets for assetsImagesAiLightedBall
+  /// assets/images/ai_lighted_ball.png
+  static const String assetsImagesAiLightedBall = "assets/images/ai_lighted_ball.png";
+
+  /// Assets for assetsImagesBackground
   /// assets/images/background.png
-  static const String resourceImagesBackground = "assets/images/background.png";
+  static const String assetsImagesBackground = "assets/images/background.png";
 
-  /// Assets for resourceImagesHollowPlayButton
-  /// assets/images/hollowPlayButton.png
-  static const String resourceImagesHollowPlayButton = "assets/images/hollowPlayButton.png";
+  /// Assets for assetsImagesHollowPlayButton
+  /// assets/images/hollow_play_button.png
+  static const String assetsImagesHollowPlayButton = "assets/images/hollow_play_button.png";
 
-  /// Assets for resourceImagesHollowRightMark
-  /// assets/images/hollowRightMark.png
-  static const String resourceImagesHollowRightMark = "assets/images/hollowRightMark.png";
+  /// Assets for assetsImagesHollowRightMark
+  /// assets/images/hollow_right_mark.png
+  static const String assetsImagesHollowRightMark = "assets/images/hollow_right_mark.png";
 
-  /// Assets for resourceImagesPersonalAvatar
-  /// assets/images/personalAvatar.png
-  static const String resourceImagesPersonalAvatar = "assets/images/personalAvatar.png";
+  /// Assets for assetsImagesMarc
+  /// assets/images/Marc.png
+  static const String assetsImagesMarc = "assets/images/Marc.png";
 
-  /// Assets for resourceImagesPlayButton
-  /// assets/images/playButton.png
-  static const String resourceImagesPlayButton = "assets/images/playButton.png";
+  /// Assets for assetsImagesPersonalAvatar
+  /// assets/images/personal_avatar.png
+  static const String assetsImagesPersonalAvatar = "assets/images/personal_avatar.png";
 
-  /// Assets for resourceImagesRightMark
-  /// assets/images/rightMark.png
-  static const String resourceImagesRightMark = "assets/images/rightMark.png";
+  /// Assets for assetsImagesPlayButton
+  /// assets/images/play_button.png
+  static const String assetsImagesPlayButton = "assets/images/play_button.png";
 
-  /// Assets for resourceTranslationsAr
+  /// Assets for assetsImagesRightMark
+  /// assets/images/right_mark.png
+  static const String assetsImagesRightMark = "assets/images/right_mark.png";
+
+  /// Assets for assetsImagesYellowTraingle
+  /// assets/images/yellow_traingle.png
+  static const String assetsImagesYellowTraingle = "assets/images/yellow_traingle.png";
+
+  /// Assets for assetsTranslationsAr
   /// assets/translations/ar.json
-  static const String resourceTranslationsAr = "assets/translations/ar.json";
+  static const String assetsTranslationsAr = "assets/translations/ar.json";
 
-  /// Assets for resourceTranslationsEn
+  /// Assets for assetsTranslationsEn
   /// assets/translations/en.json
-  static const String resourceTranslationsEn = "assets/translations/en.json";
+  static const String assetsTranslationsEn = "assets/translations/en.json";
 }
 
