9caafc8f62641445eedd31c001d360ca6e9c832e
MeronaRomany <152447527+MeronaRomany@users.noreply.github.com>
2026-05-12 04:51:51 +0300

    update ui

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -6,7 +6,7 @@ class UsersFireStore{
   UserModel userModel;
 
   Future createUserToFireStore(String uid,String name, String email)async{
-    var setuserData= await FirebaseFirestore.instance.collection("users").doc(uid);
+    var setuserData= FirebaseFirestore.instance.collection("users").doc(uid);
 
     Map<String, dynamic> json= {
       "uid": uid,
